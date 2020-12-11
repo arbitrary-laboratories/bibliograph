62229127406eadadf9bf3878eab6e7e950c5d747
GirishChandrasekar <girish.c@hotmail.com>
2020-12-10 20:03:47 -0500

    added the initial functions for the bq updater

---
+++ alexandria/bq_updater.py
@@ -0,0 +1,17 @@
+class BigQuerySchemasUpdater():
+    self.scope = "all"
+
+    def get_projects(self.scope):
+        pass
+
+    def get_tables(project):
+        pass
+
+    def get_bq_table_schema(table_name):
+        pass
+
+    def get_local_table_schema(local_path, table_name):
+        pass
+
+    def update_schema(schema, table_name):
+        pass
