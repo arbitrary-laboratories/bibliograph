b61bfe7cd9a962e655f2f20d735f9856c4978959
GirishChandrasekar <girish.c@hotmail.com>
2020-12-10 22:37:02 -0500

    added function to pull metadata from bq

--- alexandria/bq_updater.py
+++ alexandria/bq_updater.py
@@ -1,17 +1,29 @@
+from google.cloud import bigquery
+
 class BigQuerySchemasUpdater():
     self.scope = "all"
+    client = bigquery.Client()
 
-    def get_projects(self.scope):
-        pass
+    def get_tables(project, dataset):
+        # returns set of tables in the project and dataset
+        dataset_id = '{0}.{1}'.format(project, dataset)
+        tables = client.list_tables(dataset_id)
+        return set([table.table_id for table in tables])
 
-    def get_tables(project):
-        pass
+    def get_bq_table_schema(project, dataset, table_name):
+        # returns list of bq SchemaField objects
+        table_id = '{0}.{1}.{2}'.format(project, dataset, table_name)
+        table = self.get_bq_table(table_id)
+        return table.schema
 
-    def get_bq_table_schema(table_name):
-        pass
+    def get_bq_table(table_id):
+        # returns bq table object for given table_id
+        # the table_id format is 'project.dataset.table'
+        return client.get_table(table_id)
 
     def get_local_table_schema(local_path, table_name):
+
         pass
 
-    def update_schema(schema, table_name):
+    def update_table_schema(schema, table_name):
         pass
