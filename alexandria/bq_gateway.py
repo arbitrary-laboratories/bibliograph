3e6583fb510b393d4761545aa4ff582163e6cf30
GirishChandrasekar <girish.c@hotmail.com>
2020-12-13 17:40:02 -0500

    added first function to app

--- alexandria/bq_gateway.py
+++ alexandria/bq_gateway.py
@@ -1,41 +1,40 @@
 from google.cloud import bigquery
 
 class BigQueryGateway():
-    client = bigquery.Client()
+    def __init__(self):
+        self.client = bigquery.Client()
 
-    def get_tables(project, dataset):
+    def get_tables(self, project, dataset):
         # returns set of tables in the project and dataset
         dataset_id = '{0}.{1}'.format(project, dataset)
-        tables = client.list_tables(dataset_id)
-        return set([table.table_id for table in tables])
+        tables = self.client.list_tables(dataset_id)
+        return [table.table_id for table in tables]
 
-    def get_bq_table_metadata(project, dataset, table_name):
+    def get_bq_table_metadata(self, project, dataset, table_name):
         # returns list of bq SchemaField objects
         table_id = '{0}.{1}.{2}'.format(project, dataset, table_name)
-        table = client.get_table(table_id)
+        table = self.client.get_table(table_id)
         return table.schema, table.num_rows
 
-    def create_schema_object_from_json(schema_struct):
+    def create_schema_object_from_json(self, schema_struct):
         # returns a valid bigquery schema from locally storeed json representation
         schema = []
         for i in schema_struct:
             schema.append(bigquery.schema.SchemaField.from_api_repr(i))
         return schema
 
-    def update_table_schema(full_table_location, table_description, schema_struct, table_name):
+    def update_table_schema(self, full_table_location, table_description, schema_struct, table_name):
         # full_table_location: str, project.dataset.table
         # table_description: str, table description to add
         # schema_struct: list of dicts, representing a valid schema that matches the
         # schema of full_table_location with descriptions to add to each column
-        client = bigquery.Client()
-        table = client.get_table(full_table_location)
+        table = self.client.get_table(full_table_location)
         table.description = table_description
 
         schema = create_schema_object_from_json(schema_struct)
         table.schema = schema
-        client.update_table(table, ['description', 'schema'])
+        self.client.update_table(table, ['description', 'schema'])
 
-    def get_query_history():
-        client = bigquery.Client()
-        jobs = client.list_jobs(all_users=True)
+    def get_query_history(self):
+        jobs = self.client.list_jobs(all_users=True)
         return [job.query for job in jobs]
--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from alexandria.bq_gateway import BigQueryGateway
 app = Flask(__name__)
 
-
-
-@app.route("/")
-def hello_world():
-  return "Hello, World!"
-
+@app.route('/tables', methods=['GET'])
+def get_project_tables():
+    project = request.args.get('project')
+    dataset = request.args.get('dataset')
+    gateway = BigQueryGateway()
+    tables = gateway.get_tables(project, dataset)
+    return jsonify(tables)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
