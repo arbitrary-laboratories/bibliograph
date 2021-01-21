17f957c30efc962ed47f0b4ae19e83aed309e2ff
GirishChandrasekar <girish.c@hotmail.com>
2021-01-20 21:09:15 -0600

    updated the table sample fuction to return a given sample

--- exabyte/alexandria/pii_tagging/pii_service.py
+++ exabyte/alexandria/pii_tagging/pii_service.py
@@ -1,31 +1,33 @@
 from collections import Counter
+from datetime import datetime, timedelta
 import pandas as pd
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from exabyte.alexandria.utils import get_bq_gateway
+from exabyte.alexandria.listener.listener_service import ListenerService
 from exabyte.alexandria.pii_tagging.regex_pii_column_name import column_name_pii_flag
 from exabyte.alexandria.pii_tagging.regex_string_pii_scanner import column_content_pii_flag
 
 class PIIScanner(object):
-    def __init__(self, db_path, org_id, sample_size=10):
+    def __init__(self, db_path, org_id, sample_size=10, refresh_rate=48):
         self.org_id = org_id
         self.gateway = get_bq_gateway()
         self.db_path = db_path
         self.engine = create_engine('sqlite:///{path}'.format(path=self.db_path), echo=True)
         self.session = Session(self.engine)
         self.sample_size = sample_size
+        self.listener = ListenerService(self.db_path, self.org_id)
+        self.refresh_rate = refresh_rate
 
-    def get_tables(self, org_id):
-        samples = {}
-        for project in self.gateway.get_projects():
-            for dataset in self.gateway.get_datasets(project):
-                for table in self.gateway.get_tables(project, dataset):
-                    warehouse_full_table_id = "{0}:{1}.{2}".format(project, dataset, table)
-                    df = self.gateway.get_pandas_sample_from_table(project, dataset, table, self.sample_size)
-                    samples[warehouse_full_table_id] = df
-        return samples
+    def get_table_sample(self, org_id, table_id):
+        # return a sample of data for a given table_id
+        # samples = {}
+        warehouse_full_table_id = "{0}:{1}.{2}".format(project, dataset, table)
+        df = self.gateway.get_pandas_sample_from_table(project, dataset, table, self.sample_size)
+        # samples[warehouse_full_table_id] = df
+        return df
 
     def scan_column_names(self, df):
         pii_columns = set()
@@ -58,14 +60,34 @@ class PIIScanner(object):
             tables[k] = column_name_pii.union(column_content_pii)
         return tables
 
-    # def get_new_data(self, org_id):
-    #     # returns a list of full table names that were newly added
-    #     continue
-    #
-    # def get_stale_data(self, org_id):
-    #     # returns a list of full table names that were not scanned for pii recently
-    #     self.session.query(Table)
-    #
-    #
-    # def update_ebdb(self, table_name, table_pii):
-    #     table = self.session.query(TableInfo).filter_by(warehouse_full_table_id=table_name).first()
+    def get_newly_added_tables(self, org_id):
+        # returns a list of full table names that were newly added
+        local_metadata = self.listener.pull_metadata_from_ebdb(org_id)
+        dw_metadata = self.listener.pull_metadata_from_dw(org_id)
+        changes = self.listener.compare_metadata()
+        return [table['full_id'] for table in changes['add']]
+
+    def get_stale_tables(self, org_id):
+        # returns a list of full table names that were not scanned for pii recently
+        table = self.session.query(TableInfo).filter_by(org_id=org_id)
+        filter_time = datetime.utcnow() - timedelta(hours=self.refresh_rate)
+        stale_tables = table.TableInfoTag.filter_by(last_pii_scan <= filter_time)
+        return [table['warehouse_full_table_id'] for table in stale_tables]
+
+    # def update_ebdb(self, warehouse_full_table_id, pii_set, update_time):
+    #     # given a table_id and a set of columns with pii_tags, edit the ebdb
+    #     update_table = self.session.query(TableInfo).filter_by(warehouse_full_table_id = warehouse_full_table_id)
+    #     if update_table.pii_flag == False:
+    #         update_table.pii_flag = True
+    #     update_table.changed_time = update_time
+    #     update_table.version += 1
+    #     update_table.is_latest = True
+    #     update_table.pii_column_count = len(pii_set)
+    #     for column in update_table.column_infos:
+    #         if column.name in pii_set and column.pii_flag == Fals:
+    #             column.pii_flag == True
+    #             column.tag
+
+
+    def update_ebdb(self, table_name, table_pii):
+        table = self.session.query(TableInfo).filter_by(warehouse_full_table_id=table_name).first()
